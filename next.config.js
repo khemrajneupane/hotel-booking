fc21d608cf01e00d2892a256e66e18b52a123a82
Khem Raj Neupane <khemrajneupane@Khems-MacBook-Pro-2.local>
2025-03-01 23:45:23 +0200

    v2

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: "http://localhost:3000",
-        NEXTAUTH_URL:"http://localhost:3000",
-        DB_URI:"",
-        GOOGLE_CLIENT_ID:"",
-        GOOGLE_CLIENT_SECRET:"",
-        NEXTAUTH_SECRET:"",
-        CLOUDINARY_CLOUD_NAME: "",
-        CLOUDINARY_API_KEY: "",
-        CLOUDINARY_API_SECRET: "",
+        API_URL: "https://hotel-booking-drab-phi.vercel.app/",
+        NEXTAUTH_URL:"https://hotel-booking-drab-phi.vercel.app/",
+
     },
     images: {
         domains: ["res.cloudinary.com"]
