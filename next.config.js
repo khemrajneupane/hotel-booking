d37b9e311a7cde5b0b8a5f98d0c912be9b97b4bd
Khem Raj Neupane <khemrajneupane@Khems-MacBook-Pro.local>
2025-05-02 12:47:27 +0300

    next public chat api url adds

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "https://hotel-booking-khem.vercel.app",
-    NEXTAUTH_URL: "https://hotel-booking-khem.vercel.app",
+    API_URL: "http://localhost:3000/",
+    NEXT_PUBLIC_API_URL_CHAT: "",
+    CLOUDINARY_API_KEY: "",
+    CLOUDINARY_API_SECRET: "",
+    CLOUDINARY_CLOUD_NAME: "",
+    DB_URI: "",
+    GOOGLE_CLIENT_ID: "",
+    GOOGLE_CLIENT_SECRET: "",
+    NEXTAUTH_SECRET: "",
+    NEXTAUTH_URL: "http://localhost:3000/",
   },
   images: {
     domains: ["res.cloudinary.com"],
